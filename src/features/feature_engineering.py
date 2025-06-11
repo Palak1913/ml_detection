f11b5f6a54979026f120b74477e88e1004c10bcc
palak karani <capalakkarani@gmail.com>
2025-06-11 03:18:08 -0700

    tfidf vectorizer

--- src/features/feature_engineering.py
+++ src/features/feature_engineering.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 import os
 
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 import yaml
 
 max_features = yaml.safe_load(open('params.yaml','r'))['feature_engineering']['max_features']
@@ -22,7 +22,7 @@ X_test = test_data['content'].values
 y_test = test_data['sentiment'].values
 
 # Apply Bag of Words (CountVectorizer)
-vectorizer = CountVectorizer(max_features=max_features)
+vectorizer = TfidfVectorizer(max_features=max_features)
 
 # Fit the vectorizer on the training data and transform it
 X_train_bow = vectorizer.fit_transform(X_train)
